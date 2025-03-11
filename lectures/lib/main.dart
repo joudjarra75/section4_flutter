112d9d4e4e8eb33547a057d819f2df7745936e54
esraaaatmeh <joudjarrah175@gmail.com>
2025-03-11 15:05:07 +0300

    add

--- lectures/lib/main.dart
+++ lectures/lib/main.dart
@@ -2,17 +2,85 @@ import 'package:flutter/material.dart';
 import 'Screens/homeScreen.dart';
 import 'ecommerce_app/app.dart';
 void main() { //starting point
-  runApp(MyApp());
+  runApp(BaseScreenApp()
+  );
 }
 
-class MyApp extends StatelessWidget{
+class BaseScreenApp extends StatelessWidget{
+
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context){
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-     home: TAMARA(),
+      home: HomeClass(),
+
+    );
+
+  }
+
+}
+
+class HomeClass extends StatelessWidget {
+  const HomeClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      appBar: AppBar(
+        title: Text("AppBar"),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        shape: OutlineInputBorder(
+          borderSide:BorderSide(
+            // color: Colors.yellowAccent.shade200,
+            //  width: 10
+          ) ,
+          borderRadius: BorderRadius.only(bottomRight: Radius.circular(40),
+          topLeft: Radius.elliptical(50, 50))
+
+        ),
+        // shadowColor: Colors.pink,
+        elevation: 100,
+
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Text("1"),
+              Text("2"),
+              Text("3"),
+            ],
+          ), Row(
+            children: [
+              Text("4"),
+              Text("5"),
+              Text("6"),
+            ],
+          ), Row(
+            children: [
+              Text("7"),
+              Text("8"),
+              Text("9"),
+            ],
+          ),
+
+        ],
+      ),
     );
   }
 }
+
+
+
+// class MyApp extends StatelessWidget{
+//   @override
+//   Widget build (BuildContext context){
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//      home: TAMARA(),
+//     );
+//   }
+// }
 
 
